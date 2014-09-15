b2c36011545caaa4e3784dd9adf63e1cd907da47
Familie Oltmanns <yumina@Families-MacBook-Pro.local>
2014-09-15 10:14:27 +0200

    added a test class

---
+++ Git Test/MOTest.h
@@ -0,0 +1,13 @@
+//
+//  MOTest.h
+//  Git Test
+//
+//  Created by Familie Oltmanns on 15.09.14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MOTest : NSObject
+
+@end
---
+++ Git Test/MOTest.m
@@ -0,0 +1,13 @@
+//
+//  MOTest.m
+//  Git Test
+//
+//  Created by Familie Oltmanns on 15.09.14.
+//
+//
+
+#import "MOTest.h"
+
+@implementation MOTest
+
+@end
